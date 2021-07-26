6e2b27b1fd25d1c7445532522d8eaaf26755ed28
shinoburc <dandydot@gmail.com>
2021-07-26 14:14:20 +0900

    bootstrap version up.

--- gen_html.sh
+++ gen_html.sh
@@ -6,5 +6,6 @@ markdown index.md| tidy -utf8 -i > index.html
 
 sed -i -e "s/<title><\/title>/<title>${HTML_TITLE}<\/title>/" index.html
 sed -i -e "s/<head>/<head><meta charset=\"utf-8\">/" index.html
-sed -i -e "s/<head>/<head><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.6\/css\/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">/" index.html
-sed -i -e "s/<head>/<head><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.6\/css\/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l\/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">/" index.html
+#sed -i -e "s/<head>/<head><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.6\/css\/bootstrap.min.css\" integrity=\"sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7\" crossorigin=\"anonymous\">/" index.html
+#sed -i -e "s/<head>/<head><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.6\/css\/bootstrap-theme.min.css\" integrity=\"sha384-fLW2N01lMqjakBkx3l\/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r\" crossorigin=\"anonymous\">/" index.html
+sed -i -e "s/<head>/<link href=\"https:\/\/cdn.jsdelivr.net\/npm\/bootstrap@5.0.2\/dist\/css\/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-EVSTQN3\/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" crossorigin=\"anonymous\">/" index.html
